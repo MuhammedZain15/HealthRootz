bebba53481e0eb1eeb606c59666e91687aadeefe
rota259 <113599480+rota259@users.noreply.github.com>
2026-05-31 15:46:40 +0300

    port editing

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:grad_project/core/cubit/auth_cubit.dart';
 import 'package:grad_project/core/network/api_client.dart';
-import 'package:grad_project/firebase_options.dart';
 import 'package:grad_project/splash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   await ApiClient.instance.init();
   runApp(const MyApp());
 }
@@ -28,7 +23,7 @@ class MyApp extends StatelessWidget {
         title: 'Health Rootz',
         debugShowCheckedModeBanner: false,
         home: const SplashScreen(),
-      ),
+      ),ب
     );
   }
 }
